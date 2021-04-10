cd61c0b3221daeb975c043c0d3f7ef51535e9cec
rahul <rr200636@gmail.com>
2021-04-11 01:37:11 +0530

    quick sort algorithim in dart

---
+++ Dart/sort/QuickSort.dart
@@ -0,0 +1,31 @@
+
+// this function use to make partition of the list
+void swap(List list, int i, int j) {
+  int temp = list[i];
+  list[i] = list[j];
+  list[j] = temp;
+}
+
+int partition(List list, int low, int high) {
+  if (list == null || list.length == 0) return 0;
+  int pivot = list[high];
+  int i = low - 1;
+
+  for (int j = low; j < high; j++) {
+    if (list[j] <= pivot) {
+      i++;
+      swap(list, i, j);
+    }
+  swap(list, i+1, high);
+  return i+1;
+}
+
+
+// this is the driver function for quick sort
+void quickSort(List list, int low, int high) {
+  if (low < high) {
+    int pi = partition(list, low, high);
+    quickSort(list, low, pi-1);
+    quickSort(list, pi+1, high);
+  }
+}
